caaf3341cd52d5b9026e5b59ac275fdc1bf02317
david-dede <davidenedede@gmail.com>
2024-03-17 23:11:42 +0100

    banking

---
+++ konga/test.js
@@ -0,0 +1,339 @@
+let product = [
+  {
+    id: 1,
+    product_name: "Hot Choc Vending",
+    price: "¥861.09",
+    images: "image/1.jpeg",
+  },
+  {
+    id: 2,
+    product_name: "Eggplant Italian",
+    price: "¥537.05",
+    images: "image/1.jpg",
+  },
+  {
+    id: 3,
+    product_name: "Wine - Shiraz Wolf Blass Premium",
+    price: "¥536.86",
+    images: "image/1.webp",
+  },
+  {
+    id: 4,
+    product_name: "Potatoes - Idaho 80 Count",
+    price: "¥467.76",
+    images: "image/2.jpg",
+  },
+  {
+    id: 5,
+    product_name: "Cocoa Feuilletine",
+    price: "¥921.56",
+    images: "image/2.webp",
+  },
+  {
+    id: 6,
+    product_name: "Appetizer - Cheese Bites",
+    price: "¥348.46",
+    images: "image/3.webp",
+  },
+  {
+    id: 7,
+    product_name: "Cake - Pancake",
+    price: "¥55.35",
+    images: "image/1.jpg",
+  },
+  {
+    id: 8,
+    product_name: "Sambuca - Opal Nera",
+    price: "¥478.15",
+    images: "image/1.jpeg",
+  },
+  {
+    id: 9,
+    product_name: "Soup - Campbells, Creamy",
+    price: "¥125.98",
+    images: "image/2.jpg",
+  },
+  {
+    id: 10,
+    product_name: "Pork - Shoulder",
+    price: "¥174.15",
+    images: "image/1.webp",
+  },
+  {
+    id: 11,
+    product_name: "Cheese - Mozzarella",
+    price: "¥312.78",
+    images: "image/1.jpeg",
+  },
+  {
+    id: 12,
+    product_name: "Cookies - Amaretto",
+    price: "¥464.66",
+    images: "image/1.jpg",
+  },
+  {
+    id: 13,
+    product_name: "Sugar - Sweet N Low, Individual",
+    price: "¥915.31",
+    images: "image/1.webp",
+  },
+  {
+    id: 14,
+    product_name: "Burger Veggie",
+    price: "¥424.06",
+    images: "image/3.webp",
+  },
+  {
+    id: 15,
+    product_name: "Cheese - Mozzarella",
+    price: "¥434.89",
+    images: "image/2.jpg",
+  },
+  {
+    id: 16,
+    product_name: "Longos - Chicken Cordon Bleu",
+    price: "¥529.35",
+    images: "image/2.webp",
+  },
+  {
+    id: 17,
+    product_name: "Tea - Herbal Orange Spice",
+    price: "¥683.77",
+    images: "image/1.jpg",
+  },
+  {
+    id: 18,
+    product_name: "Pea - Snow",
+    price: "¥540.06",
+    images: "image/3.webp",
+  },
+  {
+    id: 19,
+    product_name: "Sobe - Liz Blizz",
+    price: "¥292.02",
+    images: "image/1.jpeg",
+  },
+  {
+    id: 20,
+    product_name: "Chicken - Breast, 5 - 7 Oz",
+    price: "¥710.98",
+    images: "image/2.jpg",
+  },
+  {
+    id: 21,
+    product_name: "Bread - Wheat Baguette",
+    price: "¥278.13",
+    images: "image/1.jpg",
+  },
+  {
+    id: 22,
+    product_name: "Beef Tenderloin Aaa",
+    price: "¥742.11",
+    images: "image/1.webp",
+  },
+  {
+    id: 23,
+    product_name: "Yeast Dry - Fleischman",
+    price: "¥167.08",
+    images: "image/1.jpeg",
+  },
+  {
+    id: 24,
+    product_name: "Wonton Wrappers",
+    price: "¥75.38",
+    images: "image/1.jpg",
+  },
+  {
+    id: 25,
+    product_name: "Wine - Magnotta - Cab Franc",
+    price: "¥493.56",
+    images: "image/1.webp",
+  },
+  {
+    id: 26,
+    product_name: "Food Colouring - Red",
+    price: "¥402.92",
+    images: "image/1.jpeg",
+  },
+  {
+    id: 27,
+    product_name: "Heavy Duty Dust Pan",
+    price: "¥659.32",
+    images: "image/2.webp",
+  },
+  {
+    id: 28,
+    product_name: "Bread Bowl Plain",
+    price: "¥637.82",
+    images: "image/3.webp",
+  },
+  {
+    id: 29,
+    product_name: "Wine - Ruffino Chianti Classico",
+    price: "¥265.92",
+    images: "image/1.jpeg",
+  },
+  {
+    id: 30,
+    product_name: "Yogurt - Strawberry, 175 Gr",
+    price: "¥495.91",
+    images: "image/1.jpg",
+  },
+  {
+    id: 31,
+    product_name: "Chicken Breast Halal",
+    price: "¥825.08",
+    images: "image/2.webp",
+  },
+  {
+    id: 32,
+    product_name: "Wine - George Duboeuf Rose",
+    price: "¥169.47",
+    images: "image/1.webp",
+  },
+  {
+    id: 33,
+    product_name: "Fish - Scallops, Cold Smoked",
+    price: "¥45.21",
+    images: "image/1.jpeg",
+  },
+  {
+    id: 34,
+    product_name: "Beef - Ground, Extra Lean, Fresh",
+    price: "¥522.70",
+    images: "image/1.jpg",
+  },
+  {
+    id: 35,
+    product_name: "Crab - Dungeness, Whole",
+    price: "¥179.45",
+    images: "image/2.jpg",
+  },
+  {
+    id: 36,
+    product_name: "Cup - 3.5oz, Foam",
+    price: "¥323.31",
+    images: "image/3.webp",
+  },
+  {
+    id: 37,
+    product_name: "Longan",
+    price: "¥563.59",
+    images: "image/2.webp",
+  },
+  {
+    id: 38,
+    product_name: "Veal - Eye Of Round",
+    price: "¥904.90",
+    images: "image/1.jpg",
+  },
+  {
+    id: 39,
+    product_name: "Vinegar - Raspberry",
+    price: "¥982.67",
+    images: "image/3.webp",
+  },
+  {
+    id: 40,
+    product_name: "Mackerel Whole Fresh",
+    price: "¥759.40",
+    images: "image/2.jpg",
+  },
+  {
+    id: 41,
+    product_name: "Wine - Pinot Grigio Collavini",
+    price: "¥439.95",
+    images: "image/3.webp",
+  },
+  {
+    id: 42,
+    product_name: "Wine - Touraine Azay - Le - Rideau",
+    price: "¥930.03",
+    images: "image/1.jpg",
+  },
+  {
+    id: 43,
+    product_name: "Muffin - Carrot Individual Wrap",
+    price: "¥248.02",
+    images: "image/1.webp",
+  },
+  {
+    id: 44,
+    product_name: "Rum - Mount Gay Eclipes",
+    price: "¥642.99",
+    images: "image/2.webp",
+  },
+  {
+    id: 45,
+    product_name: "Basil - Pesto Sauce",
+    price: "¥148.98",
+    images: "image/1.jpeg",
+  },
+  {
+    id: 46,
+    product_name: "Longos - Lasagna Beef",
+    price: "¥885.13",
+    images: "image/1.jpg",
+  },
+  {
+    id: 47,
+    product_name: "Schnappes - Peach, Walkers",
+    price: "¥122.82",
+    images: "image/1.webp",
+  },
+  {
+    id: 48,
+    product_name: "Container - Hngd Cll Blk 7x7x3",
+    price: "¥391.60",
+    images: "image/2.jpg",
+  },
+  {
+    id: 49,
+    product_name: "Pepper - Cubanelle",
+    price: "¥968.89",
+    images: "image/1.jpg",
+  },
+  {
+    id: 50,
+    product_name: "Yoghurt Tubes",
+    price: "¥838.55",
+    images: "image/1.jpeg",
+  },
+];
+
+function employeedata() {
+  const box = document.getElementById("box");
+
+  box.innerHTML = "";
+
+  product.forEach((item) => {
+    let col = document.createElement("div");
+    col.classList.add("col");
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    // Adding the image to the card body
+    let imageElement = document.createElement("img");
+    imageElement.src = item.images;
+    imageElement.classList.add("card-img-top");
+    imageElement.alt = item.product_name;
+
+    cardBody.appendChild(imageElement);
+
+    cardBody.innerHTML += `
+      <h5 class="card-title">${item.product_name}</h5>
+      <p class="card-text">Price: ${item.price}</p>
+      <P class="card-title">home:${item.product_name}</p>
+    `;
+
+    card.appendChild(cardBody);
+    col.appendChild(card);
+    box.appendChild(col);
+  });
+}
+
+window.onload = employeedata;
